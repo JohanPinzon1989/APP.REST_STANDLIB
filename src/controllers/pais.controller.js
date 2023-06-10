ad849d30010980b1947c6bdaf52c823e30016a69
Johan Enrique Pinzon Rodriguez <johan.pinzon1989@gmail.com>
2023-06-09 21:22:42 -0500

    Se  finaliza con los estados principales de consulta  tabla pais

--- src/controllers/pais.controller.js
+++ src/controllers/pais.controller.js
@@ -1,6 +1,8 @@
+import { json } from "express";
 import { getConnection } from "../database/database";
 
-const getPais = async (req, res) => {
+//Listar
+const getPaises = async (req, res) => {
   try {
     const connection = await getConnection();
     const result = await connection.query("SELECT * FROM Pais");
@@ -11,13 +13,73 @@ const getPais = async (req, res) => {
   }
 };
 
+//Buscar
+const findPais = async (req, res) => {
+  try {
+    const { Id } = req.params;
+    const connection = await getConnection();
+    const result = await connection.query(
+      "SELECT * FROM Pais WHERE Id = ?",
+      Id
+    );
+    res.json(result);
+  } catch (error) {
+    res.status(500);
+    res.send(error.message);
+  }
+};
+
+//Agregar
 const addPais = async (req, res) => {
   try {
     const { Pais, Abreviatura } = req.body;
+
+    if (Pais === undefined || Abreviatura === undefined) {
+      res.status(400),
+        json({ message: "Bad Request. Por favor ingrese todos los datos." });
+    }
+
     const pais = { Pais, Abreviatura };
     const connection = await getConnection();
-    const result = await connection.query("INSERT INTO Pais SET ?", pais);
-    res.json("res");
+    await connection.query("INSERT INTO Pais SET ?", pais);
+    res.json({ message: "Pais registrado" });
+  } catch (error) {
+    res.status(500);
+    res.send(error.message);
+  }
+};
+
+//Actuaizar
+const updatePais = async (req, res) => {
+  try {
+    const { Id } = req.params;
+    const { Pais, Abreviatura } = req.body;
+
+    if (Id === undefined || Pais === undefined || Abreviatura === undefined) {
+      res.status(400),
+        json({ message: "Bad Request. Por favor ingrese todos los datos." });
+    }
+
+    const pais = { Pais, Abreviatura };
+    const connection = await getConnection();
+    const result = await connection.query("UPDATE Pais SET ? WHERE Id = ?", [
+      pais,
+      Id,
+    ]);
+    res.json(result);
+  } catch (error) {
+    res.status(500);
+    res.send(error.message);
+  }
+};
+
+//Eliminar
+const deletePais = async (req, res) => {
+  try {
+    const { Id } = req.params;
+    const connection = await getConnection();
+    const result = await connection.query("DELETE FROM Pais WHERE Id = ?", Id);
+    res.json(result);
   } catch (error) {
     res.status(500);
     res.send(error.message);
@@ -25,6 +87,9 @@ const addPais = async (req, res) => {
 };
 
 export const methods = {
-  getPais,
+  getPaises,
+  findPais,
   addPais,
+  deletePais,
+  updatePais,
 };
--- src/routes/pais.routes.js
+++ src/routes/pais.routes.js
@@ -3,7 +3,10 @@ import { methods as paisController } from "../controllers/pais.controller";
 
 const router = Router();
 
-router.get("/", paisController.getPais);
+router.get("/", paisController.getPaises);
+router.get("/:Id", paisController.findPais);
 router.post("/", paisController.addPais);
+router.put("/:Id", paisController.updatePais);
+router.delete("/:Id", paisController.deletePais);
 
 export default router;
